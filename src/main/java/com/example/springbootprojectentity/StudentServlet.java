25427edfad199bbb8e59c3024229a3fdf14b1428
Shreypatel911 <shreypatel11999@gmail.com>
2022-06-08 14:23:51 +0530

    first commit

--- src/main/java/com/example/springbootprojectentity/StudentServlet.java
+++ src/main/java/com/example/springbootprojectentity/StudentServlet.java
@@ -21,17 +21,13 @@ public class StudentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        try{
-            StudentDetails studentDetails = new StudentDetails();
-            studentDetails.setID("1");
-            studentDetails.setName("abc");
-            studentDetails.setEmail("abc@gmail.com");
-            studentDetails.setAge(30);
+        StudentDetails studentDetails = new StudentDetails();
+        studentDetails.setID("1");
+        studentDetails.setName("abc");
+        studentDetails.setEmail("abc@gmail.com");
+        studentDetails.setAge(30);
 
-            this.datastoreTemplate.save(studentDetails);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        this.datastoreTemplate.save(studentDetails);
 
         response.getWriter().println("Hello world from Student servlet of Entity project!!! Updated code!!!");
     }
